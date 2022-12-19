23b73e18e3aab8bf5d3b4faca90850e4c02d3db3
Roberto Ramirez <robert.624@hotmail.com>
2022-12-19 16:00:27 -0500

    details component finished

--- components/index.js
+++ components/index.js
@@ -2,5 +2,8 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
 
-export { FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RectButton };
+export { FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RectButton, SubInfo, DetailsDesc, DetailsBid };
